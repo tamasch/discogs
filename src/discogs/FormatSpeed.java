e7e5182c7bfffa6a009c3f84bd25130a4ca6d0fd
tamasch <pocokt@gmail.com>
2017-01-19 18:17:30 +0100

    commit test

--- src/discogs/FormatSpeed.java
+++ src/discogs/FormatSpeed.java
@@ -8,5 +8,15 @@ package discogs;
  *
  */
 public enum FormatSpeed {
-
+	_8_1_2("8 1/2 RPM"),
+	_16_2_3("16 2/3 RPM"),
+	_33_1_3("33 1/3 RPM"),
+	_45("45 RPM"),
+	_78("78 RPM");
+	
+	private String speed;
+	
+	FormatSpeed(String speed){
+		this.speed = speed;
+	}
 }
